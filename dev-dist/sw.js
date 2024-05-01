f5dc8cee1dc73100fb2addbcdeb26ba5a5f2019c
ravinderpalK <ravinderpalsinghkhokhar@gmail.com>
2024-05-01 23:42:28 +0530

    .

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pfa8n0a4b1g"
+    "revision": "0.dtmohbfsqfo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
